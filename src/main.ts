efbc8ee1a9c4f8f3f8ecfe606baf9e1f19b8dcb8
Alexeis Carrillo <alexeis.carrillo@gmail.com>
2021-10-20 20:15:18 -0400

    servicios https

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  console.log(__dirname + `/cert/www_jih-service_com_key.pem`);
+  const httpsOptions = {
+    key: fs.readFileSync(__dirname + `/cert/www_jih-service_com.key`),
+    cert: fs.readFileSync(__dirname + `/cert/www_jih-service_com.crt`),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+  //const app = await NestFactory.create(AppModule);
   app.enableCors();
 
   const options = new DocumentBuilder()
